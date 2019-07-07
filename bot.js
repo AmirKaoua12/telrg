cf2bd8dda2b6d2deda90096bbce242a28a843ea9
AmirKaoua12 <51892471+AmirKaoua12@users.noreply.github.com>
2019-07-07 22:30:55 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("597517573697044531")
+setInterval(function() {
+channel.send(`Amir GX credits plz give me you are the best bro main i hate you evry match tow meinuts and i go to schoool ohhh yeh Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX Amir GX  No plz sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
